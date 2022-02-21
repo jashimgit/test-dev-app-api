a5aa9e48d2a17545133922f06fcf6072b09c4320
jashimgit <jashim.ncn@gmail.com>
2022-02-21 18:55:50 +0600

    first commit

---
+++ src/routes/auth.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* typescript-eslint/no-unsafe-assignment */
+import { Router, Request, Response, NextFunction } from "express";
+import passport from "passport";
+
+import loginValidation from "../validations/auth/login";
+import { googleLogin, login, logout, refresh } from "../controllers/auth";
+import { validate } from "../controllers/utils";
+import { isAuthenticated } from "../middlewares/auth";
+import User from "../models/user";
+import { handleError, handleOk } from "../middlewares/response";
+const router = Router();
+
+router.post(
+  "/login",
+  loginValidation,
+  (request: Request, response: Response, next: NextFunction) => {
+    validate(request).then(next).catch(next);
+  },
+  (request: Request, response: Response, next: NextFunction) => {
+    login(request).then(next).catch(next);
+  }
+);
+
+router.post(
+  "/google",
+  passport.authenticate("google-token", {
+    session: false,
+    scope: ["profile", "email"],
+  }),
+  (request: Request, response: Response, next: NextFunction) => {
+    googleLogin(request).then(next).catch(next);
+  }
+);
+
+router.get(
+  "/logout",
+  isAuthenticated("member"),
+  (request: Request, response: Response, next: NextFunction) => {
+    logout(request).then(next).catch(next);
+  }
+);
+
+router.put(
+  "/refresh",
+  (request: Request, response: Response, next: NextFunction) => {
+    refresh(request).then(next).catch(next);
+  }
+);
+
+export default router;
